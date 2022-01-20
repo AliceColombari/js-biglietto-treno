5a83197185ecdc3ac0186896da65b34ef68681f5
AliceColombari <colombari.23@gmail.com>
2022-01-20 14:14:24 +0100

    prove condizione + prezzofinale

--- javascript/main.js
+++ javascript/main.js
@@ -22,3 +22,40 @@ console.log(pricePerKm);
 // prezzo biglietto = km inseriti * prezzo per km
 const priceTicket = (trip * pricePerKm);
 console.log(priceTicket);
+
+// sconto applicato in base all'età
+// prezzo
+let price = '';
+// sconto 20% per i minorenni
+let sale20 = (priceTicket * 20/ 100);
+// sconto 40% per over 65
+let sale40 = (priceTicket * 40/ 100);
+// nessuno sconto per altri
+let noSale = 0;
+
+// condizione sconto applicato in base all'età
+if (userAge < 18) {
+    price = sale20;
+    console.log('Sconto Minorenne: ' + price);
+
+} else if (userAge > 65) {
+    price = sale40;
+    console.log('Sconto Over65: ' + price);
+
+} else {
+    price = noSale;
+    console.log('Sconto Maggiorenne: ' + price);
+}
+
+
+// prezzo finale = prezzo biglietto - sconto applicato
+let finalPrice = (priceTicket - price);
+// arrotondamento del prezzo con massimo due decimali
+const roundedPrice = finalPrice.toFixed(2);
+
+
+// calcolo finale in base ai dati
+let totalPrice = 'Il costo del tuo biglietto è: ' + roundedPrice + ' €';
+console.log(totalPrice);
+
+
